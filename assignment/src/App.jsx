1d4d6c2529f27fdef56913597b1452f2a22792c2
legit92 <nitindhake3@gmail.com>
2025-02-18 13:11:15 +0530

    completed

---
+++ assignment/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>My Name: [Nitin]</h1>
+      <p>Email: [nitin@gmail.com]</p>
+    </div>
+  );
+}
+
+export default App;
